f684141b346be353aa204b1e44c1abebfc797ca8
Victor Santelé <Vic16@hotmail.be>
2021-11-11 19:57:10 +0100

    first commit

---
+++ server/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+read_var() {
+  VAR=$(grep $1 $2 | xargs)
+  IFS="=" read -ra VAR <<< "$VAR"
+  echo ${VAR[1]}
+}
+
+USERNAME=wolfvic.azurecr.io
+IMAGE=dataweek
+VERSION=$(read_var VERSION .env)
+
+
+docker build -t $USERNAME/$IMAGE:latest -t $USERNAME/$IMAGE:$VERSION .
---
+++ server/server.js
@@ -0,0 +1,96 @@
+const path = require("path");
+require("dotenv").config();
+const logger =
+  process.env.NODE_ENV !== "production"
+    ? true
+    : {
+        file: path.join(__dirname, "data/dataweek.log"),
+        level: "info",
+        sync: true,
+      };
+const fastify = require("fastify")({ logger });
+const fastifyCors = require("fastify-cors");
+const fastifyStatic = require("fastify-static");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+const postEndpoints = [
+  "gender",
+  "hairColor",
+  "hairCut",
+  "mask",
+  "shoe",
+  "shoesColor",
+  "top",
+  "trouser",
+];
+const optPost = {
+  schema: {
+    params: {
+      type: "object",
+      properties: {
+        category: { type: "string", enum: postEndpoints },
+      },
+    },
+  },
+};
+
+fastify.post("/api/:category", optPost, async (request, reply) => {
+  const { name } = request.body;
+  const category = request.params.category;
+  await prisma.category.upsert({
+    where: { category_name: { category: category, name: name } },
+    update: { count: { increment: 1 } },
+    create: { category, name, count: 1 },
+  });
+  reply.send({ message: "Success" });
+});
+
+const getEndpoints = [
+  "genders",
+  "hairColors",
+  "hairCuts",
+  "masks",
+  "shoes",
+  "shoesColors",
+  "tops",
+  "trousers",
+];
+const optGet = {
+  schema: {
+    params: {
+      type: "object",
+      properties: {
+        category: { type: "string", enum: getEndpoints },
+      },
+    },
+  },
+};
+
+fastify.get("/api/:category", optGet, async (request, reply) => {
+  const category = request.params.category.slice(0, -1);
+  const res = await prisma.category.findMany({
+    where: { category },
+    select: { name: true, count: true },
+  });
+  reply.send(res);
+});
+
+const start = async () => {
+  try {
+    await fastify.register(fastifyCors);
+    fastify.register(fastifyStatic, {
+      root: path.join(__dirname, "dist"),
+      prefix: "/",
+    });
+    fastify.get("/", (request, reply) => {
+      reply.sendFile("index.html");
+    });
+    fastify.listen(4448);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
