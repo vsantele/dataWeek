a24168c709928ed6ffeacf3df918c34cd45ea255
Victor Santelé <Vic16@hotmail.be>
2021-11-15 23:41:59 +0100

    add liquid engine

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ import dotenv from "dotenv";
 import Fastify from "fastify";
 import fastifyCors from "fastify-cors";
 import fastifyStatic from "fastify-static";
+import fs from "fs";
+import { Liquid } from "liquidjs";
 import path from "path";
+import PointOfView from "point-of-view";
 import UAParser from "ua-parser-js";
 dotenv.config();
 const __dirname = path.resolve();
@@ -19,6 +22,21 @@ const logger =
 const fastify = Fastify({ logger });
 const { PrismaClient } = Prisma;
 const prisma = new PrismaClient();
+
+const engine = new Liquid({
+  root: path.join(__dirname, "views"),
+  extname: ".liquid",
+  cache: process.env.NODE_ENV === "production",
+});
+fastify.register(PointOfView, {
+  engine: {
+    liquid: engine,
+  },
+});
+
+const infos = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "data/categoryInfos.json"))
+);
 
 const postEndpoints = [
   "gender",
@@ -53,14 +71,19 @@ fastify.post("/api/:category", optPost, async (request, reply) => {
 });
 
 const getEndpoints = [
-  "genders",
-  "hairColors",
-  "hairCuts",
-  "masks",
-  "shoes",
-  "shoesColors",
-  "tops",
-  "trousers",
+  "gender",
+  "hairColor",
+  "hairCut",
+  "mask",
+  "shoe",
+  "shoesColor",
+  "top",
+  "trouser",
+  "browser",
+  "os",
+  "deviceType",
+  "deviceVendor",
+  "deviceModel",
 ];
 const optGet = {
   schema: {
@@ -74,13 +97,52 @@ const optGet = {
 };
 
 fastify.get("/api/:category", optGet, async (request, reply) => {
-  const category = request.params.category.slice(0, -1);
+  const category = request.params.category;
   const res = await prisma.category.findMany({
     where: { category },
     select: { name: true, count: true },
     orderBy: { name: "asc" },
   });
   reply.send(res);
+});
+
+fastify.get("/charts/:category", optGet, async (request, reply) => {
+  const category = request.params.category;
+  const data = await prisma.category.findMany({
+    where: { category },
+    select: { name: true, count: true },
+    orderBy: { name: "asc" },
+  });
+  const specials = [
+    "browser",
+    "os",
+    "deviceType",
+    "deviceVendor",
+    "deviceModel",
+  ];
+  let info = {};
+  if (specials.includes(category)) {
+    info.choices = data.map((d) => ({
+      code: d.name,
+      name: d.name,
+    }));
+  } else {
+    info = infos[category];
+  }
+  return reply.view("/views/chart.liquid", {
+    category,
+    name: info.name,
+    infos: info.choices,
+    data: JSON.stringify(data),
+  });
+});
+fastify.get("/collectors/:category", optGet, async (request, reply) => {
+  const category = request.params.category;
+  return reply.view("/views/collector.liquid", {
+    category,
+    name: infos[category].name,
+    infos: infos[category].choices,
+  });
 });
 
 fastify.get("/quizz", async (request, reply) => {
@@ -93,34 +155,34 @@ fastify.get("/quizz", async (request, reply) => {
   const deviceVendor = ua.device?.vendor ?? "other";
   const deviceModel = ua.device?.model ?? "other";
   let category = "browser";
-  prisma.category.upsert({
+  await prisma.category.upsert({
     where: { category_name: { category, name: browser } },
     update: { count: { increment: 1 } },
     create: { category, name: browser, count: 1 },
   });
   category = "os";
-  prisma.category.upsert({
+  await prisma.category.upsert({
     where: { category_name: { category, name: os } },
     update: { count: { increment: 1 } },
-    create: { category, name: browser, count: 1 },
+    create: { category, name: os, count: 1 },
   });
   category = "deviceType";
-  prisma.category.upsert({
+  await prisma.category.upsert({
     where: { category_name: { category, name: deviceType } },
     update: { count: { increment: 1 } },
-    create: { category, name: browser, count: 1 },
+    create: { category, name: deviceType, count: 1 },
   });
   category = "deviceVendor";
-  prisma.category.upsert({
+  await prisma.category.upsert({
     where: { category_name: { category, name: deviceVendor } },
     update: { count: { increment: 1 } },
-    create: { category, name: browser, count: 1 },
+    create: { category, name: deviceVendor, count: 1 },
   });
   category = "deviceModel";
-  prisma.category.upsert({
+  await prisma.category.upsert({
     where: { category_name: { category, name: deviceModel } },
     update: { count: { increment: 1 } },
-    create: { category, name: browser, count: 1 },
+    create: { category, name: deviceModel, count: 1 },
   });
 });
 
